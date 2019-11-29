0a277fa07b5c37cf9bb68a8d566b674832c9149a
abel1502 <terragame.abel@gmail.com>
2019-11-29 12:57:41 +0300

    Tail

---
+++ engine.py
@@ -0,0 +1,463 @@
+import pygame
+import math
+from collections import deque
+from enum import Enum
+import sys
+
+
+class Vector2:
+    def __init__(self, *args):
+        if len(args) == 0:
+            self = Vector2(0, 0)
+        elif len(args) == 1:
+            if isinstance(args[0], (tuple, list)):
+                assert len(args[0]) == 2
+                assert isinstance(args[0][0], (int, float)) and isinstance(args[0][1], (int, float))
+                self.x = args[0][0]
+                self.y = args[0][1]
+            elif isinstance(args[0], Vector2):
+                self.x = args[0].x
+                self.y = args[0].y
+            else:
+                assert False
+        elif len(args) == 2:
+            assert isinstance(args[0], (int, float)) and isinstance(args[1], (int, float))
+            self.x = args[0]
+            self.y = args[1]
+        else:
+            assert False
+    
+    def tuple(self):
+        return (self.x, self.y)
+    
+    def __round__(self, ndigits=0):
+        return Vector2(round(self.x, ndigits=ndigits), round(self.y, ndigits=ndigits))
+    
+    def __str__(self):
+        return "({}, {})".format(self.x, self.y)
+    
+    def __repr__(self):
+        return "Vector2({}, {})".format(self.x, self.y)
+    
+    def __complex__(self):
+        return complex(self.x, self.y)
+    
+    def __abs__(self):
+        return math.hypot(self.x, self.y)
+    
+    def angle(self, other=None):
+        if other is None:
+            return math.atan2(self.y, self.x)
+        assert isinstance(other, Vector2)
+        return math.atan2(self @ other, self * other)
+    
+    @staticmethod
+    def fromPolar(r, phi):
+        return Vector2(r * math.cos(phi), r * math.sin(phi))
+    
+    def toPolar(self):
+        return (abs(self), self.angle())
+    
+    def copy(self):
+        return Vector2(self)
+    
+    def normalize(self):
+        return self / abs(self)
+    
+    def lerp(self, other, time=0.5):
+        return self * (1 - time) + other * time
+    
+    def _rotate(self, sin, cos, rel=None):
+        if rel is None:
+            return Vector2(self.x * cos - self.y * sin, self.x * sin + self.y * cos)
+        return (self - rel)._rotate(sin, cos) + rel
+    
+    def rotate(self, phi, rel=None):
+        return self._rotate(math.sin(phi), math.cos(phi), rel=rel)
+    
+    def rotate90(self, rel=None):
+        return self._rotate(1, 0, rel=rel)
+    
+    def clamp(self, rect, loop=True):
+        if loop:
+            return Vector2((self.x - rect.x) % rect.width + rect.x, (self.y - rect.y) % rect.height + rect.y)
+        return Vector2(max(rect.left, min(self.x, rect.right)), max(rect.top, min(self.y, rect.bottom)))
+    
+    @staticmethod
+    def rect(v1, v2):
+        x1, y1 = round(v1).tuple()
+        x2, y2 = round(v2).tuple()
+        size = tuple(map(abs, round(v2 - v1).tuple()))
+        return pygame.Rect((min(x1, x2), min(y1, y2)), size)
+    
+    # ===[ Operators ]===
+    
+    def __add__(self, other):
+        if isinstance(other, Vector2):
+            return Vector2(self.x + other.x, self.y + other.y)
+        raise NotImplementedError()
+    
+    def __sub__(self, other):
+        if isinstance(other, Vector2):
+            return self + -other
+        raise NotImplementedError()
+    
+    def __mul__(self, other):
+        if isinstance(other, Vector2):
+            return Vector2(self.x * other.x, self.y * other.y)
+        if isinstance(other, (int, float)):
+            return Vector2(self.x * other, self.y * other)
+        raise NotImplementedError()
+    
+    def __matmul__(self, other):
+        if isinstance(other, Vector2):
+            return self.x * other.y + self.y * other.x
+        raise NotImplementedError()
+    
+    def __truediv__(self, other):
+        if isinstance(other, (int, float)):
+            return self * (1 / other)
+        raise NotImplementedError()
+    
+    def __rmul__(self, other):
+        if isinstance(other, Vector2):
+            return self * other
+        if isinstance(other, (int, float)):
+            return self * other
+        raise NotImplementedError()
+    
+    def __iadd__(self, other):
+        self = self + other
+        return self
+    
+    def __isub__(self, other):
+        self = self - other
+        return self
+    
+    def __imul__(self, other):
+        self = self * other
+        return self
+    
+    def __imatmul__(self, other):
+        self = self @ other
+        return self
+    
+    def __itruediv__(self, other):
+        self = self / other
+        return self
+    
+    def __neg__(self):
+        return Vector2(-self.x, -self.y)
+    
+    def __pos__(self):
+        return Vector2(self.x, self.y)
+    
+    # ===[ Comparisons ]===
+    
+    def __eq__(self, other):
+        if not isinstance(other, Vector2):
+            raise NotImplementedError()
+        return self.x == other.x and self.y == other.y
+    
+    def __ne__(self, other):
+        return not self == other
+    
+    def __lt__(self, other):
+        return self.toRPhi() < other.toRPhi()
+    
+    def __le__(self, other):
+        return self < other or self == other
+    
+    def __gt__(self, other):
+        return other >= self
+    
+    def __ge__(self, other):
+        return other > self
+
+
+#class IndexedContainer:
+#    def __init__(self):
+#        self.elements = {}
+#        self.lastId = -1
+#    
+#    def add(self, elem):
+#        self.lastId += 1
+#        self.set(self.lastId, elem)
+#        return self.lastId
+#    
+#    def pop(self, id):
+#        return self.elements.pop(id)
+#    
+#    def clear(self):
+#        self.elements = {}
+#    
+#    def set(self, id, elem):
+#        self.elements[id] = elem
+#    
+#    def get(self, id):
+#        return self.elements[id]
+#    
+#    def getAll(self, withIds=False):
+#        if withIds:
+#            return self.elements.items()
+#        return self.elements.values()
+
+
+class Component:
+    def __init__(self, gameObject):
+        self.gameObject = gameObject
+
+
+class GameObject:
+    def __init__(self, name=None):
+        self.name = name
+        self.components = set()
+        self.tags = set()
+    
+    def instantiate(self, parent, *args, **kwargs):
+        self.addComponent(Transform, parent=parent.transform, *args, **kwargs)
+    
+    @property
+    def transform(self):
+        return self.getComponent(Transform)
+    
+    def destroy(self):
+        pass  # TODO!
+    
+    def assignParent(self, parent):
+        assert isinstance(parent, GameObject)
+        self.transform.assignParent(parent.transforn)
+    
+    def addChild(self, child):
+        assert isinstance(child, GameObject)
+        self.transform.addChild(child.transform)
+    
+    def removeChild(self, child):
+        assert isinstance(child, GameObject)
+        self.transform.removeChild(child.transform)
+    
+    def clearChildren(self):
+        self.transform.clearChildren()
+    
+    def getChildren(self):
+        return set(map(lambda x: x.gameObject, self.transform.getChildren()))
+    
+    def getParent(self):
+        return self.transform.getParent().gameObject
+    
+    def getRoot(self):
+        return self.transform.getRoot().gameObject
+    
+    def findChildByName(self, name):
+        return self.transform.findChildByName(name).gameObject
+    
+    def findChildByTag(self, tag):
+        return self.transform.findChildByTag(tag).gameObject
+    
+    def findChildrenByTag(self, tag):
+        return list(map(lambda x: x.gameObject, self.transform.findChildrenByTag(tag)))
+    
+    def broadcastMessage(self, msg, *args, targetComponent=Component, **kwargs):
+        self.transform.broadcastMessage(msg, *args, targetComponent=targetComponent, **kwargs)
+    
+    def addComponent(self, comp, *args, **kwargs):
+        comp = comp(self, *args, **kwargs)
+        assert isinstance(comp, Component)
+        self.components.add(comp)
+    
+    def removeComponent(self, comp):
+        assert isinstance(comp, Component)
+        self.components.discard(comp)
+    
+    def clearComponents(self):
+        self.components.clear()
+    
+    def getComponents(self, type=Component):
+        res = []
+        for comp in self.components:
+            if isinstance(comp, type):
+                res.append(comp)
+        return res
+    
+    def getComponent(self, type=Component):
+        for comp in self.components:
+            if isinstance(comp, type):
+                return comp
+        assert False
+    
+    def handleMessage(self, msg, *args, targetComponent=Component, **kwargs):
+        handler = "on_{}".format(msg)
+        #print(self.name, handler)
+        for comp in self.getComponents(targetComponent):
+            if hasattr(comp, handler):
+                getattr(comp, handler)(*args, **kwargs)
+
+
+class Transform(Component):
+    def __init__(self, gameObject, parent=None, pos=(0, 0), priority=0):
+        super().__init__(gameObject)
+        self.priority = priority
+        self.pos = Vector2(pos)
+        self.parent = None
+        self.assignParent(parent)
+        self.children = set()
+    
+    def assignParent(self, parent):
+        if self.parent is not None:
+            self.parent.removeChild(self)
+        if parent is None:
+            self.parent = None
+            return
+        assert isinstance(parent, Transform)
+        parent.addChild(self)
+        self.parent = parent
+    
+    def addChild(self, child):
+        assert isinstance(child, Transform)
+        self.children.add(child)
+    
+    def removeChild(self, child):
+        assert isinstance(child, Transform)
+        self.children.discard(child)
+    
+    def clearChildren(self):
+        self.children.clear()
+    
+    def getChildren(self):
+        return sorted(self.children, key=lambda x: x.priority)
+    
+    def getParent(self):
+        return self.parent
+    
+    def getRoot(self):
+        if self.parent is not None:
+            return self.parent.getRoot()
+        return self
+    
+    def findChildByName(self, name):
+        for child in self.getChildren():
+            if child.gameObject.name == name:
+                return child
+        assert False
+    
+    def findChildByTag(self, tag):
+        for child in self.getChildren():
+            if tag in child.gameObject.tags:
+                return child
+        assert False
+    
+    def findChildrenByTag(self, tag):
+        ans = []
+        for child in self.getChildren():
+            if tag in child.gameObject.tags:
+                ans.append(child)
+        return ans
+    
+    def broadcastMessage(self, msg, *args, targetComponent=Component, **kwargs):
+        self.gameObject.handleMessage(msg, *args, targetComponent=targetComponent, **kwargs)
+        for child in self.getChildren():
+            child.broadcastMessage(msg, *args, targetComponent=targetComponent, **kwargs)
+    
+    def getPosition(self):
+        return self.pos  # ?.copy()
+    
+    def getAbsolutePosition(self):
+        if self.parent is None:
+            return self.getPosition()
+        return self.getPosition() + self.parent.getAbsolutePosition()
+    
+    def getRelativePosition(self, other):
+        return self.getAbsolutePosition() - other.getAbsolutePosition()
+    
+    def toAbsolute(self, coords):
+        return coords + self.getAbsolutePosition()
+    
+    def fromAbsolute(self, coords):
+        return coords - self.getAbsolutePosition()
+    
+    def toRelative(self, other, coords):
+        return coords + self.getRelativePosition(other)
+    
+    def fromRelative(self, other, coords):
+        return coords - self.getRelativePosition(other)
+
+
+class Behaviour(Component):
+    pass
+
+
+class Texture(Component):
+    def __init__(self, gameObject, zIndex=0):
+        super().__init__(gameObject)
+        self.zIndex = zIndex
+    
+    def draw(self, drawBuf, color, rect):
+        drawBuf.append((color, rect, self.zIndex))
+
+
+class BoxTexture(Texture):
+    def __init__(self, gameObject, size, color, zIndex=0, offset=(0, 0)):
+        super().__init__(gameObject, zIndex=zIndex)
+        self.offset = Vector2(offset)
+        self.color = color
+        self.size = size
+    
+    def getRect(self):
+        pos = round(self.gameObject.transform.getAbsolutePosition() + self.offset)
+        return pygame.Rect(pos.tuple(), self.size)
+    
+    def on_draw(self, drawBuf):
+        self.draw(drawBuf, self.color, self.getRect())
+
+
+class Collider(Component):
+    pass
+
+
+class BoxCollider(Collider):
+    pass
+
+
+class Scene(GameObject):
+    def __init__(self, size=(800, 600), fpsLimit=1000, bgColor=(255, 255, 255)):
+        super().__init__("Scene")
+        self.screen = pygame.display.set_mode(size)
+        self.clock = pygame.time.Clock()
+        self.fpsLimit = fpsLimit
+        self.bgColor = bgColor
+        self.drawBuf = []
+        self.addComponent(Transform)
+    
+    def handleEvent(self, event):
+        tr = self.transform
+        if event.type == pygame.QUIT:
+            self.stop()
+        if event.type == pygame.KEYDOWN:
+            tr.broadcastMessage("keydown", event.key, event.mod, event.unicode, event.scancode)
+        if event.type == pygame.KEYUP:
+            tr.broadcastMessage("keyup", event.key, event.mod)
+        pass  # TODO: Implement the rest!!
+    
+    def run(self):
+        tr = self.transform
+        tr.broadcastMessage("start")
+        while True:
+            self.clock.tick(self.fpsLimit)
+            self.drawBuf = []
+            for event in pygame.event.get():
+                self.handleEvent(event)
+            tr.broadcastMessage("tick", self)
+            self.screen.fill(self.bgColor)
+            tr.broadcastMessage("draw", self.drawBuf, targetComponent=Component)
+            self.drawAll()
+    
+    def drawAll(self):
+        for color, rect, _ in sorted(self.drawBuf, key=lambda x: x[2]):
+            surface = pygame.Surface(rect.size)
+            surface.fill(color)
+            self.screen.blit(surface, rect)
+        pygame.display.update()
+    
+    def stop(self):
+        pygame.quit()
+        sys.exit()
--- main.py
+++ main.py
@@ -1,52 +1,128 @@
 import pygame
-import sys
-import maths
+from engine import *
+import random
+from collections import deque
+
+
+class SnakeController(Behaviour):
+    def on_start(self):
+        self.speed = 0.15
+        self.moveDir = Vector2(1, 0)
+        self.head = Head("snakeHead", 1)
+        self.head.instantiate(self.gameObject, pos=Vector2(0, 0))
+        self.tail = Tail("snakeTail", 0.5)
+        self.tail.instantiate(self.gameObject, pos=Vector2(0, 0))
+        self.length = 0
+        self.food = Food("food", -1)
+        self.food.instantiate(self.gameObject.getRoot())
+        self.eat()
+        
+    def eat(self):
+        self.length += 1
+        scene = self.gameObject.getRoot()
+        foodSize = self.food.getComponent(BoxTexture).size
+        _x = random.random() * (scene.screen.get_rect().width - foodSize[0])
+        _y = random.random() * (scene.screen.get_rect().height - foodSize[1])
+        self.food.transform.pos = Vector2(_x, _y)
+    
+    def on_keydown(self, key, mod, unicode, scancode):
+        newMoveDir = self.moveDir
+        if key in [pygame.K_w, pygame.K_UP]:
+            newMoveDir = Vector2(0, -1)
+        elif key in [pygame.K_s, pygame.K_DOWN]:
+            newMoveDir = Vector2(0, 1)
+        elif key in [pygame.K_a, pygame.K_LEFT]:
+            newMoveDir = Vector2(-1, 0)
+        elif key in [pygame.K_d, pygame.K_RIGHT]:
+            newMoveDir = Vector2(1, 0)
+        if newMoveDir @ self.moveDir == 0:
+            return
+        self.moveDir = newMoveDir
+    
+    def on_tick(self, scene):
+        tr = self.head.transform
+        tr.pos += self.moveDir * self.speed * scene.clock.get_time()
+        tr.pos = tr.parent.fromAbsolute(tr.getAbsolutePosition().clamp(scene.screen.get_rect()))
+        self.tail.addTrail(tr.pos)
+        # TODO: Collider Component
+        headCollider = self.head.getComponent(BoxTexture).getRect()
+        foodCollider = self.food.getComponent(BoxTexture).getRect()
+        if headCollider.colliderect(foodCollider):
+            self.eat()
+
+
+class Head(GameObject):
+    def __init__(self, name=None, zIndex=0):
+        super().__init__(name)
+        self.addComponent(BoxTexture, (32, 32), (255, 255, 255), zIndex=zIndex, offset=(-16, -16))
+
+
+class Food(GameObject):
+    def __init__(self, name=None, zIndex=0):
+        super().__init__(name)
+        self.addComponent(BoxTexture, (16, 16), (255, 0, 0), zIndex=zIndex, offset=(-8, -8))
+
+
+class TailCollider(Collider):
+    pass
+
+
+class TailTexture(Texture):
+    def __init__(self, gameObject, width, color, zIndex=0):
+        super().__init__(gameObject, zIndex=zIndex)
+        self.color = color
+        self.width = width
+    
+    def on_draw(self, drawBuf):
+        trail = list(self.gameObject.trail)[::-1]
+        rem = self.gameObject.getParent().getComponent(SnakeController).length * 32
+        for i in range(1, len(trail)):
+            prevPoint = trail[i - 1][0]
+            curPoint = trail[i][0]
+            segLen = abs(curPoint - prevPoint)
+            rem -= segLen
+            dir = (curPoint - prevPoint).normalize()
+            radius = dir.rotate90() * (self.width / 2)
+            if rem <= 0:
+                r0 = prevPoint - radius - dir
+                r1 = curPoint + dir * rem + radius + dir
+                rect = Vector2.rect(r0, r1)
+                self.draw(drawBuf, self.color, rect)
+                break
+            r0 = prevPoint - radius - dir
+            r1 = curPoint + radius + dir
+            rect = Vector2.rect(r0, r1)
+            self.draw(drawBuf, self.color, rect)
+
+
+class Tail(GameObject):
+    def __init__(self, name=None, zIndex=0):
+        super().__init__(name)
+        self.trail = deque()
+        self.addComponent(TailTexture, 32, (0, 255, 0), zIndex=zIndex)
+    
+    def addTrail(self, pos, food=False):
+        if len(self.trail) >= 2:
+            lastDir = self.trail[-1][0] - self.trail[-2][0]
+            curDir = pos - self.trail[-1][0]
+            if curDir @ lastDir == 0:
+                if not self.trail[-1][1]:
+                    self.trail.pop()
+        self.trail.append((pos, food))
+        # !Clear
+        if len(self.trail) >= 10 ** 5:
+            self.trail.popleft()
 
 
 def main():
     pygame.init()
-    size = width, height = 800, 600
+    size = 800, 600
     
-    screen = pygame.display.set_mode(size)
-    clock = pygame.time.Clock()
-    maxFps = 1000
-    
-    BLACK = (0, 0, 0)
-    WHITE = (255, 255, 255)
-    RED = (255, 0, 0)
-    GREEN = (0, 255, 0)
-    BLUE = (0, 0, 255)
-    
-    image = pygame.Surface((32, 32))
-    image.fill(WHITE)
-    rect = image.get_rect()
-    
-    velocity = 0.25
-    moveDir = maths.Vector2(1, 0)
-    pos = maths.Vector2(0, 0)
-    while True:
-        deltaTime = clock.tick(maxFps)
-        pos += moveDir * velocity * deltaTime
-        pos = pos.clamp(screen.get_rect())
-        rect.x, rect.y = round(pos).tuple()
-    
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            elif event.type == pygame.KEYDOWN:
-                if event.key in [pygame.K_w, pygame.K_UP]:
-                    moveDir = maths.Vector2(0, -1)
-                elif event.key in [pygame.K_s, pygame.K_DOWN]:
-                    moveDir = maths.Vector2(0, 1)
-                elif event.key in [pygame.K_a, pygame.K_LEFT]:
-                    moveDir = maths.Vector2(-1, 0)
-                elif event.key in [pygame.K_d, pygame.K_RIGHT]:
-                    moveDir = maths.Vector2(1, 0)
-    
-        screen.fill(BLACK)
-        screen.blit(image, rect)
-        pygame.display.update()
+    scene = Scene(size=size, bgColor=(0, 0, 0))
+    snake = GameObject()
+    snake.addComponent(SnakeController)
+    snake.instantiate(scene)
+    scene.run()
 
 try:
     main()
